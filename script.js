bde91b86116cfb526f8eb65999c99e7871959677
Vetrivel M.D <44906792+Vetrivel-Hari@users.noreply.github.com>
2022-09-26 14:35:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function successCallBack(token){
-    console.log(token);
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch(`https://www.google.com/recaptcha/api/siteverify?secret=6LfeQS8iAAAAAGrK5_TOGq2DUeQjXqQmHxV0pQSz&response=${token}`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
